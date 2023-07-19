6f8c43d4f967149086c9779552338679eabe2bf7
Edward <eghuffstetler@gmail.com>
2023-07-19 17:09:20 -0400

    styling for edit queue

--- react-app/src/components/Main/MusicBar/DisplayQueue.js
+++ react-app/src/components/Main/MusicBar/DisplayQueue.js
@@ -44,36 +44,38 @@ export default function DisplayQueue() {
 
     return (
         <div className="display-queue__outer-div">
-            <h3>{user.username}'s queue</h3>
+            <h1>{user.username}'s queue</h1>
             <div className="display-queue__current">
                 {currentSong.id ?
                     <>
-                        <p>Now playing</p>
-                        <p>Name: {currentSong.name}</p>
-                        <p>Artist: {currentSong.owner.username}</p>
-                        <p>description: {currentSong.description}</p>
+                        <p className="display-queue__now">Now playing</p>
+                        <div className="display-queue__current-song">
+                            <div>
+                                <p className="display-queue__current-title">{currentSong.name}</p>
+                                <p className="display-queue__auther">{currentSong.owner.username}</p>
+                            </div>
+                            <p className="display-queue__description">{currentSong.description}</p>
+                        </div>
                     </>
                     :
                     <p>No current song</p>
                 }
             </div>
-            <ul>
-                {nextSongs.length == 0 && "No songs in queue"}
+            <ul className="display-queue__ul">
+                {nextSongs.length == 0 ? <p className="display-queue__now">No songs in queue</p> : <p className="display-queue__now">Songs in queue</p>}
                 {nextSongs.map((song, i) => (
                     <li key={Math.random()} className="display-queue__li">
-                        <div>
-                            <p>{song.name} by {song.owner.username}</p>
-                            <p>{song.description}</p>
-                        </div>
-                        <div>
-                            <button onClick={() => handleMoveForward(i)} disabled={i === nextSongs.length - 1}>Move forward</button>
-                            <button onClick={() => handleMoveBackward(i)} disabled={i == 0}>Move backward</button>
-                            <button onClick={() => handleDelete(i)}>Delete</button>
+                        <p className="display-queue__li-title">{song.name}</p>
+                        <p className="display-queue__li-auther">By {song.owner.username}</p>
+                        <div className="display-queue__li-buttons">
+                            {i !== nextSongs.length - 1 ? <i onClick={() => handleMoveForward(i)} className="display-queue__forward fa-solid fa-arrow-down"></i> : <i className="display-queue__forward"></i>}
+                            {i !== 0 ? <i onClick={() => handleMoveBackward(i)} className="display-queue__back fa-solid fa-arrow-up"></i> : <i className="display-queue__back"></i>}
+                            <i onClick={() => handleDelete(i)} className="display-queue__delete fa-solid fa-trash"></i>
                         </div>
                     </li>
                 ))}
             </ul>
-            <div>
+            <div className="display-queue__buttons-div">
                 <button onClick={closeModal}>Cancel</button>
                 <button onClick={handleSave}>Save</button>
             </div>
