a6a279359faff91638c54241490ea8d07d17927c
Edward <eghuffstetler@gmail.com>
2023-07-19 15:22:44 -0400

    styling for upload song

--- react-app/src/components/Main/SongForm/index.js
+++ react-app/src/components/Main/SongForm/index.js
@@ -35,43 +35,53 @@ export default function SongForm() {
     }
 
     return (
-        <div>
-            <h1>Upload a new song</h1>
-            {loading && <h3>File uploading ...</h3>}
-            <form
-                onSubmit={handleSubmit}
-                encType="multipart/form-data"
-            >
-                <label>
-                    Song Name
-                    <input
-                        value={songName}
-                        onChange={(e) => setSongName(e.target.value)}
-                        maxLength={50}
-                        minLength={2}
-                    />
-                    {songName.length >= 50 && <p>Song name max length reached</p>}
-                </label>
-                <p className="validation-error">{validationErrors.name}</p>
-                <input
-                    type="file"
-                    accept="audio/*"
-                    onChange={(e) => {setSongData(e.target.files[0])}}
-                />
-                 <p className="validation-error">{validationErrors.song_file}</p>
-                <label>
-                    Song Description
-                    <input
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        maxLength={100}
-                        minLength={5}
-                    />
-                    {description.length >= 100 && <p>Song description max length reached</p>}
-                </label>
-                <p className="validation-error">{validationErrors.description}</p>
-                <button type="submit">Upload Song</button>
-            </form>
+        <div className='song-form__outer'>
+            <div className='song-form__form-wrapper'>
+                <h1>Upload a new song</h1>
+                {loading && <h3>File uploading ...</h3>}
+                <form className='song-form__form'
+                    onSubmit={handleSubmit}
+                    encType="multipart/form-data"
+                >
+                    <label>
+                        Song Name
+                        <input
+                            className='song-form__input'
+                            value={songName}
+                            onChange={(e) => setSongName(e.target.value)}
+                            maxLength={50}
+                            minLength={2}
+                        />
+                        {songName.length >= 50 && <p className='song-form__maxlen'>Song name max length reached</p>}
+                    </label>
+                    <p className="validation-error">{validationErrors.name}</p>
+                    <label>
+                        Audio File
+                        <input
+                            className='song-form__file'
+                            type="file"
+                            accept="audio/*"
+                            onChange={(e) => {setSongData(e.target.files[0])}}
+                        />
+                    </label>
+                    <p className="validation-error">{validationErrors.song_file}</p>
+                    <label>
+                        Song Description
+                        <input
+                            className='song-form__input'
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            maxLength={100}
+                            minLength={5}
+                        />
+                        {description.length >= 100 && <p className='song-form__maxlen'>Song description max length reached</p>}
+                    </label>
+                    <p className="validation-error">{validationErrors.description}</p>
+                    <div className='song-form__button-div'>
+                        <button className='song-form__button' type="submit">Upload Song</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
