d190839d0a697046bb4604ac49e93082865da92c
Edward <eghuffstetler@gmail.com>
2023-07-23 14:59:23 -0400

    added in production code for migration

--- migrations/versions/20230720_103526_initial_migration.py
+++ migrations/versions/20230720_103526_initial_migration.py
@@ -1,12 +1,17 @@
 """initial migration
 
 Revision ID: 414081b0c3e1
-Revises: 
+Revises:
 Create Date: 2023-07-20 10:35:26.893636
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -68,6 +73,13 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE feeds SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE follows SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE songs SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE queues SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -27,15 +27,20 @@ export default function Splash() {
                 </div>
             </div>
             <div className="splash__bottom-links">
-                <div>
-                    <p>Github link</p>
-                    <p>Linkden link</p>
+                <div className='splash__bottom-corners'>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://github.com/Edward932" }}>Github</Link>
+                    <Link className="splash__button-links" to={{ pathname: "https://www.linkedin.com/in/edwardhuffstetler/" }}>LinkedIn</Link>
                 </div>
-                <div>
-                    List of technology used (ALL OF THESE ARE DEAD LINK FOR NOW)
+                <div className='splash__bottom-middle'>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://react.dev/" }}>React</Link>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://flask.palletsprojects.com/en/2.3.x/" }}>Flask</Link>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://aws.amazon.com/" }}>AWS</Link>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://redux.js.org/" }}>Redux</Link>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" }}>JavaScript</Link>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://www.python.org/" }}>Python</Link>
                 </div>
-                <div>
-                    <p>Link to repository</p>
+                <div className='splash__bottom-corners'>
+                    <Link className="splash__button-links" target="_blank" to={{ pathname: "https://github.com/Edward932/mod7-spotifly-clone" }}>Link to repository</Link>
                 </div>
             </div>
         </div>
